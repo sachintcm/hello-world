646bbe76632649f4f27d3789ccbaa0b881683c2a
sachintcm <sachitcn.tcm2692@gmail.com>
2024-07-12 09:45:00 +0530

    added subtract fuction to helloWorld

--- Hello.c
+++ Hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int add(int,int);
+int sub(int,int);
+
 
 int main()
 {
@@ -15,10 +17,17 @@ int main()
     printf("\nEnter B: ");
     scanf("%d",&b);
     printf("A+B = %d\n",add(a,b));
+    printf("A-B = %d\n",sub(a,b));
+
     return 0;
 }
 
 int add (int a, int b)
 {
     return(a+b);
+}
+
+int sub(int a,int b)
+{
+    return(a-b);
 }
