7865da711e36036114d5f4cb50765819c742687b
sachintcm <sachitcn.tcm2692@gmail.com>
2024-07-12 08:56:40 +0530

    Modified helloworld program with addition operation

--- Hello.c
+++ Hello.c
@@ -3,8 +3,15 @@
 int main()
 {
     char a[10];
+    int a;
+    int b;
     printf("Enter your name\n");
     scanf("%s", a);
     printf("%s\n",a);
+    printf("Enter A: ");
+    scanf("%d",&a);
+    printf("\nEnter B: ");
+    scanf("%d",&b);
+    printf("A+B = %d\n",(a+b));
     return 0;
 }
