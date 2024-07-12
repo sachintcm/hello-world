5422bd80ae29088fede35b3fdddd053ce95e94a3
sachintcm <sachitcn.tcm2692@gmail.com>
2024-07-12 09:25:51 +0530

    added function fot the add operation in hello world

--- Hello.c
+++ Hello.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
+int add(int,int);
+
 int main()
 {
-    char a[10];
+    char name[10];
     int a;
     int b;
     printf("Enter your name\n");
-    scanf("%s", a);
+    scanf("%s", name);
     printf("%s\n",a);
     printf("Enter A: ");
     scanf("%d",&a);
     printf("\nEnter B: ");
     scanf("%d",&b);
-    printf("A+B = %d\n",(a+b));
+    printf("A+B = %d\n",add(a,b));
     return 0;
+}
+
+int add (int a, int b)
+{
+    return(a+b);
 }
